39e6dbdaa0b2526377127860b638fabe50efbd86
Randy Hucker <randalhucker@gmail.com>
2024-10-12 22:06:42 -0400

    Working Init
    - Added line to supply actionable item
    - Changed Readme

--- examples/scenario_new.py
+++ examples/scenario_new.py
@@ -89,20 +89,22 @@ game_settings = {
     "graphics_mode": GraphicsType.Tkinter,
     "realtime_multiplier": 1,
 }
-game = KesslerGame(settings=game_settings)  # Use this to visualize the game scenario
-# game = TrainerEnvironment(settings=game_settings)  # Use this for max-speed, no-graphics simulation
 
-pre = time.perf_counter()
-chrom = [2, 2, 2, 0, 1, 1, 0, 0, 1, 2, 2, 2, 0, 1, 1, 0, 0, 1]
-chrom = [int(x) for x in chrom]
-score, perf_data = game.run(
-    scenario=scenario_random_repeatable,
-    controllers=[FuzzyController()]
-)
+if __name__ == "__main__":
+    game = KesslerGame(settings=game_settings)  # Use this to visualize the game scenario
+    # game = TrainerEnvironment(settings=game_settings)  # Use this for max-speed, no-graphics simulation
 
-print("Scenario eval time: " + str(time.perf_counter() - pre))
-print(score.stop_reason)
-print("Asteroids hit: " + str([team.asteroids_hit for team in score.teams]))
-print("Deaths: " + str([team.deaths for team in score.teams]))
-print("Accuracy: " + str([team.accuracy for team in score.teams]))
-print("Mean eval time: " + str([team.mean_eval_time for team in score.teams]))
+    pre = time.perf_counter()
+    chrom = [2, 2, 2, 0, 1, 1, 0, 0, 1, 2, 2, 2, 0, 1, 1, 0, 0, 1]
+    chrom = [int(x) for x in chrom]
+    score, perf_data = game.run(
+        scenario=scenario_random_repeatable,
+        controllers=[FuzzyController()]
+    )
+
+    print("Scenario eval time: " + str(time.perf_counter() - pre))
+    print(score.stop_reason)
+    print("Asteroids hit: " + str([team.asteroids_hit for team in score.teams]))
+    print("Deaths: " + str([team.deaths for team in score.teams]))
+    print("Accuracy: " + str([team.accuracy for team in score.teams]))
+    print("Mean eval time: " + str([team.mean_eval_time for team in score.teams]))
