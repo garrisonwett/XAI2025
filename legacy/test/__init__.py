fb960aac60079a18c4fc924fd90e448e6c517003
Garrison Wettengel <115752606+garrisonwett@users.noreply.github.com>
2025-04-24 09:26:44 -0400

    Removed Legacy

--- legacy/test/__init__.py
+++
@@ -1,2 +0,0 @@
-# This directory can be used to build unit tests for your project
-# This is very beneficial for collaborating on code as a team
--- legacy/test/test_controller.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-
-class ControllerTests(unittest.TestCase):
-    def example(self):
-        # This should return no errors because True==True
-        self.assertTrue(True)
